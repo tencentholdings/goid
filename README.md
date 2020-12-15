15d0dc887f28a65d7d22b168a67ac479dec1944a
tencentholdings <vanctardi@protonmail.com>
2020-12-15 00:50:47 -0500

    API Tests

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # GOID (GOG Offline Installer Downloader)
-GOID is an attempt at a script to automatically check for updates in your game library. If a newer version is detected, it automatically downloads the offline installer and replaces the older version with the newer version.
+GOID is an attempt at a script to automatically check for updates in your game library using the GOG API. If a newer version is detected, it automatically downloads the offline installer and replaces the older version with the newer version.
 
 This is my first project and first attempt at using Python, as well as my first time playing around with an API. If you have any improvements you'd like to suggest, feel free to do so!
+
+## Installation
+You can simply run main.py or the executable provided in the "releases" section.
+
+## Required info
+- Login (token)
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import tkinter as tk
+import requests as rq
 
 authTokenValue = None
 
+response = rq.get("cdn.gog.com/content-system/v1/manifests/1207658930/windows/37794096/463cd4b2-783e-447a-b17e-a68d601911e3.json")
+print(response.status_code)
+print(response.json())
 
 def test_function():
-	print(authToken.get())
 	authTokenValue = authToken.get()
 	label = tk.Label(frame, text = "AuthToken: " + str(authTokenValue))
 	label.place(relx=0.05, rely=0.8)
 
 root = tk.Tk()
-root.title("GOG Offline Installer Downloader")
+root.title("GOG Offline Installer Downloader -")
 root.geometry("600x600")
 
 canvas = tk.Canvas(root, height=600, width = 600, bg="#e6e6e6")
@@ -22,11 +25,10 @@ frame.place(relwidth=1, relheight=0.5)
 label = tk.Label(frame, text = "AuthToken: " + str(authTokenValue))
 label.place(relx=0.05, rely=0.8)
 
-authToken = tk.Entry(frame, bg="blue")
+authToken = tk.Entry(frame, bg="#ccffff")
 authToken.place(relx=0.05, rely=0.05)
 
-updateButton = tk.Button(frame, bg = "red", text = "Update", command=test_function)
+updateButton = tk.Button(frame, bg = "#ff8080", text = "Update", command=test_function)
 updateButton.place(relx = 0.05, rely = 0.15)
-
 
 root.mainloop()
